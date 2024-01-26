17efd45edb42ceec53dcac3eee83d130f6aa41b0
hadhi hassan m <diluhadhi01@gmail.com>
2024-01-26 05:18:35 -0800

    First commit

---
+++ ts/src/App.tsx
@@ -0,0 +1,15 @@
+import './index.css';
+import './App.css';
+function App() {
+
+  return (
+    <>
+    
+     <div>
+     
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ ts/src/components/imageHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const imageHeader = () => {
+    return <div>
+
+        <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+        </div>
+    </div>;
+}
+
+export default imageHeader;
