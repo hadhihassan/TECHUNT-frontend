8736d290c78fe04d46d9e2c783cfeb8be9354199
hadhi hassan m <diluhadhi01@gmail.com>
2024-04-18 17:35:41 -0700

    feat:Review , reSheduling addedd.

--- ts/src/components/General/Home/content/dummy.tsx
+++ ts/src/components/General/Home/content/dummy.tsx
@@ -8,10 +8,10 @@ const dummy = () => {
                 <div className="w-[650px] h-[70px] mt-5 text-white text-xl sm:text-sm  md:text-4xl xl:text-5xl   font-normal font-montserrat break-all ">Join world’s best market place
                 </div>
                 <div className="text-white text-base font-medium  leading-[33px] break-all">Find the best Talent and best works based on your skills from around the world.</div>
-                <div className="grid grid-cols-12 w-[40vh] gap-2 content-center">
+                {/* <div className="grid grid-cols-12 w-[40vh] gap-2 content-center">
                     <button className="w-[124px] sm:col-span-12 md:col-span-12   col-span-12    h-[37px] bg-black text-white rounded-[20px] text-sm">Find works</button>
                     <button className="w-[124px] h-[37px] border rounded-[20px]  text-white text-sm">Find talent</button>
-                </div>
+                </div> */}
             </div> 
             <div className=' xl:w-[30%] md:w-[30%] sm:w-full sm:mb-10  flex items-center justify-center'>
                 <img src={sdf} alt="sdhfjsh" />
