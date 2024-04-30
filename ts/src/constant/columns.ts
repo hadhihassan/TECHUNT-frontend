fde5352aa725cfa5787606fa1e026adeb54eeb85
hadhi hassan m <diluhadhi01@gmail.com>
2024-04-30 01:24:16 -0700

    fix the cors

--- ts/src/constant/columns.ts
+++ ts/src/constant/columns.ts
@@ -8,10 +8,10 @@ export interface JOB_CATEGORY_FORM_DATA {
     description: string,
 }
 
-// export const IMG_URL = "https://timezones.website/images/"
-export const IMG_URL = "http://localhost:3000/images/"
-// export const BASE_URL = "https://timezones.website"
-export const BASE_URL = "http://localhost:3000"
+export const IMG_URL = "https://timezones.website/images/"
+// export const IMG_URL = "http://localhost:3000/images/"
+export const BASE_URL = "https://timezones.website"
+// export const BASE_URL = "http://localhost:3000"
 export const TABLE_HEAD = ["No", "Title", "Amount", "Date", "Recived / Payed"];
 export const INDIAN_RUPEE = "₹";
 
